882554a754c239a40fe2fecaa4f07050bb4c4062
Bibek Saini <itsbibeksaini@gmail.com>
2022-08-06 23:47:43 +0530

    chore: kotlin class example

--- projects/kotlin-poet/src/main/kotlin/com/examples/kotlinpoet/KotlinClass.kt
+++ projects/kotlin-poet/src/main/kotlin/com/examples/kotlinpoet/KotlinClass.kt
@@ -3,9 +3,12 @@ package com.examples.kotlinpoet
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.TypeVariableName
 
+/**
+ * Sample kotlin poet to generate kotlin class file.
+ */
 fun main(args: Array<String>) {
-
     val sampleClass = TypeSpec
         .classBuilder("TestClass")
         .primaryConstructor(
@@ -14,6 +17,8 @@ fun main(args: Array<String>) {
                 .addParameter("test", String::class)
                 .build()
         )
+        .addTypeVariable(TypeVariableName.Companion.invoke("var1", String::class))
+        .addFunction(FunSpec.builder("testFun").addStatement("return \"test func\"").returns(String::class).build())
         .build()
     val kotlinFile: FileSpec = FileSpec.builder("com.example.testpackage", "TestClass").addType(sampleClass).build()
 
